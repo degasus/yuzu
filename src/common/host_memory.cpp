cbcb15370b068187598453d4ac176f2f0fbb51b0
Markus Wick <markus@selfnet.de>
2020-01-19 01:49:30 +0100

    common: Create a new host memory helper.

---
+++ src/common/host_memory.cpp
@@ -0,0 +1,253 @@
+#ifdef __linux__
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#elif defined(_WIN32)
+#include <fileapi.h>
+#include <memoryapi.h>
+#include <processthreadsapi.h>
+#pragma comment(lib, "Kernel32.lib")
+#endif
+
+#include <cassert>
+#include <cstring>
+#include "common/host_memory.h"
+
+static size_t total_memory_used = 0;
+
+namespace Common {
+
+#ifdef __linux__
+
+class HostMemory::Impl {
+public:
+    Impl(std::string_view usage) : fd(memfd_create(std::string(usage).c_str(), 0)) {}
+
+    ~Impl() {
+        close(fd);
+    }
+
+    bool resize(size_t bytes) {
+        int err = ftruncate(fd, bytes);
+        if (err != 0)
+            return false;
+
+        if (size) {
+            err = munmap(pointer, size);
+            assert(err == 0);
+        }
+
+        if (bytes) {
+            pointer =
+                static_cast<u8*>(mmap(nullptr, bytes, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0));
+            if (pointer == nullptr) {
+                return false;
+            }
+        } else {
+            pointer = nullptr;
+        }
+
+        size = bytes;
+
+        return true;
+    }
+
+    void clearRegion(size_t length, size_t offset) {
+        int err = fallocate(fd, FALLOC_FL_PUNCH_HOLE | FALLOC_FL_KEEP_SIZE, offset, length);
+        (void)err;
+        assert(err == 0);
+    }
+
+    bool map(size_t length, size_t offset, u8* pointer) {
+        void* ret =
+            mmap(pointer, length, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_FIXED, fd, offset);
+        assert(ret == nullptr || ret == pointer);
+
+        return ret != nullptr;
+    }
+
+    bool unmap(size_t length, u8* pointer) {
+        int ret = munmap(pointer, length);
+
+        return ret == 0;
+    }
+
+    size_t size = 0;
+    u8* pointer = nullptr;
+    const int fd;
+};
+
+#elif defined(_WIN32)
+
+class Impl {
+    Impl() {
+        handle = CreateFileMapping(INVALID_HANDLE_VALUE, nullptr, PAGE_READWRITE, 0, 0, nullptr);
+        assert(handle != nullptr);
+    }
+
+    ~Impl() {
+        CloseHandle(handle);
+        handle = 0;
+    }
+
+    bool resize(size_t bytes) {
+        bool success = SetFilePointerEx(handle, bytes, nullptr, FILE_BEGIN) && SetEndOfFile(handle);
+        if (!success)
+            return false;
+
+        if (size) {
+            success = UnmapViewOfFile(pointer);
+            assert(success);
+        }
+
+        if (bytes) {
+            pointer = MapViewOfFile(handle, FILE_MAP_ALL_ACCESS, 0, 0, bytes);
+        } else {
+            pointer = nullptr;
+        }
+
+        size = bytes;
+
+        return true;
+    }
+
+    void clearRegion(size_t length, size_t offset) {
+        // TODO: implement me
+        std::memset(pointer + offset, 0, length);
+    }
+
+    bool map(size_t length, size_t offset, u8* pointer) {
+        u8* ret = MapViewOfFileEx(handle, FILE_MAP_ALL_ACCESS, 0, offset, length, pointer);
+
+        return ret == pointer;
+    }
+
+    bool unmap(size_t length, u8* pointer) {
+        return UnmapViewOfFile(pointer);
+    }
+
+    size_t size = 0;
+    u8* pointer = nullptr;
+    HANDLE handle;
+};
+
+#else
+
+#error Please implement the host memory for your platform
+
+#endif
+
+HostMemory::HostMemory(std::string_view usage) : usage(usage) {}
+
+HostMemory::~HostMemory() {
+    // Free all memory
+    resize(0);
+}
+
+HostMemory::HostMemory(size_t bytes, std::string_view usage) : HostMemory(usage) {
+    resize(bytes);
+}
+
+HostMemory::HostMemory(HostMemory&& other) : HostMemory(std::string_view{}) {
+    impl.swap(other.impl);
+    usage.swap(other.usage);
+}
+
+HostMemory& HostMemory::operator=(HostMemory&& other) {
+    // Free all memory
+    resize(0);
+
+    impl.swap(other.impl);
+
+    return *this;
+}
+
+bool HostMemory::resize(size_t bytes) {
+    // size does not change, so nothing to do
+    if (bytes == size())
+        return true;
+
+    // buffer was empty, so create the implementation
+    if (!impl)
+        impl = std::make_unique<Impl>(usage);
+
+    total_memory_used -= size();
+
+    bool success = impl->resize(bytes);
+
+    total_memory_used += bytes;
+
+    printf("resizing HostMemory fd %d to %zx bytes, mapped to %p -> %d, total memory usage: %zd "
+           "bytes\n",
+           impl->fd, bytes, impl->pointer, success, total_memory_used);
+
+    // buffer shall be of zero bytes, so free it
+    if (!bytes)
+        impl.reset();
+
+    return success;
+}
+
+void HostMemory::clearRegion(size_t length, size_t offset) {
+    assert(length + offset <= size());
+
+    if (!length)
+        return;
+
+    impl->clearRegion(length, offset);
+
+    printf("clearing HostMemory fd %d, %zx bytes at %zx offset\n", impl->fd, length, offset);
+}
+
+bool HostMemory::map(size_t length, size_t offset, u8* pointer) {
+    assert(length + offset <= size());
+
+    if (!length)
+        return true;
+
+    bool success = impl->map(length, offset, pointer);
+
+    printf("map HostMemory fd %d, %zx bytes at %zx offset to %p (base ptr %p) -> %d\n", impl->fd,
+           length, offset, pointer, pointer - offset, success);
+
+    return success;
+}
+
+bool HostMemory::unmap(size_t length, u8* pointer) {
+    assert(length + offset <= size());
+
+    if (!length)
+        return true;
+
+    bool success = impl->unmap(length, pointer);
+
+    printf("unmap HostMemory fd %d, %zx bytes from %p -> %d\n", impl->fd, length, pointer, success);
+
+    return success;
+}
+
+size_t HostMemory::size() const {
+    if (!impl)
+        return 0;
+
+    return impl->size;
+}
+
+u8* HostMemory::data() {
+    if (!impl)
+        return nullptr;
+
+    return impl->pointer;
+}
+
+const u8* HostMemory::data() const {
+    if (!impl)
+        return nullptr;
+
+    return impl->pointer;
+}
+
+} // namespace Common
---
+++ src/common/host_memory.h
@@ -0,0 +1,99 @@
+// Copyright 2019 yuzu Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include <memory>
+#include "common/common_types.h"
+
+namespace Common {
+
+/**
+ * A low level linear memory buffer, which supports multiple mappings
+ * Its purpose is to rebuild a given sparse memory layout, including mirrors.
+ */
+class HostMemory {
+public:
+    HostMemory(std::string_view usage = "HostMemory");
+    ~HostMemory();
+
+    /**
+     * Constructor with inlined memory allocation
+     * This is identical to calling resize(bytes) on a default created object.
+     */
+    explicit HostMemory(size_t bytes, std::string_view usage = "HostMemory");
+
+    /**
+     * Copy constructors. They shall return a copy of the buffer without the mappings.
+     * TODO: Implement them with COW if needed.
+     */
+    HostMemory(HostMemory& other) = delete;
+    HostMemory& operator=(HostMemory& other) = delete;
+
+    /**
+     * Move constructors. They will move the buffer and the mappings to the new object.
+     */
+    HostMemory(HostMemory&& other);
+    HostMemory& operator=(HostMemory&& other);
+
+    /**
+     * Expand of shrink the buffer
+     * On expanding, it will be extended with (sparse) zeros.
+     * On shrinking, overlapping mappings will be cleared.
+     *
+     * This call might fail if the system runs out of memory.
+     */
+    bool resize(size_t bytes);
+
+    /**
+     * Zeros a region of the buffer
+     * Compared to memset, this method will try to generate sparse pages to save physical memory.
+     *
+     * @pre length + offset must not be larger than size
+     */
+    void clearRegion(size_t length, size_t offset);
+
+    /**
+     * Try to map a region of the buffer to a given pointer
+     *
+     * Length, offset and pointer must be page aligned (4k on linux, 64k on windows).
+     *
+     * @pre length + offset must not be larger than size
+     * @return true of the mapping was successful
+     */
+    bool map(size_t length, size_t offset, u8* pointer);
+
+    /**
+     * Unmap (partial) region(s) of the buffer
+     * This method unmaps all overlapping mappings of this buffer.
+     *
+     * Length and pointer must be page aligned (4k on linux, 64k on windows).
+     *
+     * @pre length + offset must not be larger than size
+     * @return true if any mapping was cleared
+     */
+    bool unmap(size_t length, u8* pointer);
+
+    /**
+     * Query the current size of the buffer
+     * @return the size of the buffer in bytes
+     */
+    size_t size() const;
+
+    /**
+     * Query the pointer of the internal storage
+     * This pointer will be invalidated by the next resize call.
+     * @return pointer to the begin of the internal storage
+     */
+    u8* data();
+    const u8* data() const;
+
+private:
+    // Low level handler for the platform dependent memory routines
+    class Impl;
+    std::unique_ptr<Impl> impl;
+    std::string usage;
+};
+
+} // namespace Common
