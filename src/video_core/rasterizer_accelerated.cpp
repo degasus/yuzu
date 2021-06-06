1f2cb942a7c3b49d7adb46cf13c1d18d33b34b41
Rodrigo Locatti <rodrigo.locatti@gmail.com>
2021-06-07 00:27:47 +0200

    Rasterizer: Accumulate page updates.

--- src/video_core/rasterizer_accelerated.cpp
+++ src/video_core/rasterizer_accelerated.cpp
@@ -2,6 +2,8 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
+#include <atomic>
+
 #include "common/assert.h"
 #include "common/common_types.h"
 #include "common/div_ceil.h"
@@ -10,35 +12,59 @@
 
 namespace VideoCore {
 
-RasterizerAccelerated::RasterizerAccelerated(Core::Memory::Memory& cpu_memory_)
-    : cpu_memory{cpu_memory_} {}
+using namespace Core::Memory;
+
+RasterizerAccelerated::RasterizerAccelerated(Memory& cpu_memory_) : cpu_memory{cpu_memory_} {}
 
 RasterizerAccelerated::~RasterizerAccelerated() = default;
 
 void RasterizerAccelerated::UpdatePagesCachedCount(VAddr addr, u64 size, int delta) {
-    const auto page_end = Common::DivCeil(addr + size, Core::Memory::PAGE_SIZE);
-    for (auto page = addr >> Core::Memory::PAGE_BITS; page != page_end; ++page) {
-        auto& count = cached_pages.at(page >> 2).Count(page);
+    u64 uncache_begin = 0;
+    u64 cache_begin = 0;
+    u64 num_uncache = 0;
+    u64 num_cache = 0;
+
+    std::atomic_thread_fence(std::memory_order_acquire);
+    const u64 page_end = Common::DivCeil(addr + size, PAGE_SIZE);
+    for (u64 page = addr >> PAGE_BITS; page != page_end; ++page) {
+        std::atomic_uint16_t& count = cached_pages.at(page >> 2).Count(page);
 
         if (delta > 0) {
-            ASSERT_MSG(count < UINT16_MAX, "Count may overflow!");
+            ASSERT_MSG(count.load(std::memory_order::relaxed) < UINT16_MAX, "Count may overflow!");
         } else if (delta < 0) {
-            ASSERT_MSG(count > 0, "Count may underflow!");
+            ASSERT_MSG(count.load(std::memory_order::relaxed) > 0, "Count may underflow!");
         } else {
-            ASSERT_MSG(true, "Delta must be non-zero!");
+            ASSERT_MSG(false, "Delta must be non-zero!");
         }
 
         // Adds or subtracts 1, as count is a unsigned 8-bit value
-        count += static_cast<u16>(delta);
+        count.fetch_add(static_cast<u16>(delta), std::memory_order_release);
 
         // Assume delta is either -1 or 1
-        if (count == 0) {
-            cpu_memory.RasterizerMarkRegionCached(page << Core::Memory::PAGE_BITS,
-                                                  Core::Memory::PAGE_SIZE, false);
-        } else if (count == 1 && delta > 0) {
-            cpu_memory.RasterizerMarkRegionCached(page << Core::Memory::PAGE_BITS,
-                                                  Core::Memory::PAGE_SIZE, true);
+        if (count.load(std::memory_order::relaxed) == 0) {
+            if (num_uncache == 0) {
+                uncache_begin = page;
+            }
+            num_uncache += PAGE_SIZE;
+        } else if (num_uncache > 0) {
+            cpu_memory.RasterizerMarkRegionCached(uncache_begin << PAGE_BITS, num_uncache, false);
+            num_uncache = 0;
         }
+        if (count.load(std::memory_order::relaxed) == 1 && delta > 0) {
+            if (num_cache == 0) {
+                cache_begin = page;
+            }
+            num_cache += PAGE_SIZE;
+        } else {
+            cpu_memory.RasterizerMarkRegionCached(cache_begin << PAGE_BITS, num_cache, true);
+            num_cache = 0;
+        }
+    }
+    if (num_uncache > 0) {
+        cpu_memory.RasterizerMarkRegionCached(uncache_begin << PAGE_BITS, num_uncache, false);
+    }
+    if (num_cache > 0) {
+        cpu_memory.RasterizerMarkRegionCached(cache_begin << PAGE_BITS, num_cache, true);
     }
 }
 
